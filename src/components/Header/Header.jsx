edf53d70905113a15a8be54c16b936e53f3f9ff7
leeyeun <dpdms0109@naver.com>
2022-08-20 02:00:47 +0900

    Feat: top Header생성

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+function Header() {
+    return (
+        <div className="topMainMenu">
+            <header className="topHeader">
+                <nav className="topNav">
+                    <h1 className="topLogo"></h1>
+                    <input
+                        type="text"
+                        className="topSearch"
+                        placeholder="상품을 검색해보세요!"
+                    />
+                </nav>
+                <ul className="topUl">
+                    <li className="cart">
+                        <Link to="/cart">장바구니</Link>
+                    </li>
+                    <li className="login">
+                        <Link to="/login">로그인</Link>
+                    </li>
+                </ul>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import "./Home.css";
+function Home() {
+    return (
+        <div className="page">
+            <Header />
+            <div>Home</div>
+        </div>
+    );
+}
+
+export default Home;
