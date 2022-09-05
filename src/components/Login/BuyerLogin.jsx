2e7821123847ba5542fabe58fb9320fff7eac3ff
leeyeun <dpdms0109@naver.com>
2022-09-05 19:27:06 +0900

    Feat : 로그인 구매회원, 판매회원 페이지 분리

---
+++ src/components/Login/BuyerLogin.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginForm from "./LoginForm";
+
+function BuyerLogin() {
+    return (
+        <div>
+            <LoginForm />
+            <p>구매</p>
+        </div>
+    );
+}
+
+export default BuyerLogin;
---
+++ src/components/Login/SellerLogin.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginForm from "./LoginForm";
+
+function SellerLogin() {
+    return (
+        <div>
+            {/* <button>구매회원 로그인</button> */}
+            <LoginForm />
+            <p>판매</p>
+        </div>
+    );
+}
+
+export default SellerLogin;
