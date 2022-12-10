46033e567c30d82d8dcc34c87c4c690e60cce535
leeyeun <dpdms0109@naver.com>
2022-12-10 23:51:20 +0900

    Feat : 로그인 페이지 로고 클릭시 홈 이동

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,36 +3,46 @@ import { useState } from "react";
 import BuyerLogin from "./BuyerLogin";
 import LoginController from "./LoginController";
 import SellerLogin from "./SellerLogin";
-
+import LogoImage from "../../assets/Logo-hodu.png";
+import "./Login.css";
 function Login() {
-    const [buyerClick, setBuyerClick] = useState(true);
-    const [sellerClick, setSellerClick] = useState(false);
+  const [buyerClick, setBuyerClick] = useState(true);
+  const [sellerClick, setSellerClick] = useState(false);
 
-    const onClickBuyer = () => {
-        setBuyerClick(true);
-        setSellerClick(false);
-    };
+  const onClickBuyer = () => {
+    setBuyerClick(true);
+    setSellerClick(false);
+  };
 
-    const onClickSeller = () => {
-        setBuyerClick(false);
-        setSellerClick(true);
-    };
+  const onClickSeller = () => {
+    setBuyerClick(false);
+    setSellerClick(true);
+  };
 
-    return (
-        <div>
-            <LoginController
-                buyerClick={buyerClick}
-                sellerClick={sellerClick}
-                onClickBuyer={onClickBuyer}
-                onClickSeller={onClickSeller}
-            />
-            {buyerClick === true && sellerClick === false ? (
-                <BuyerLogin />
-            ) : (
-                <SellerLogin />
-            )}
-        </div>
-    );
+  const ongoHome = () => {
+    window.location = "/";
+  };
+  return (
+    <div>
+      <img
+        src={LogoImage}
+        alt="logo"
+        className="loginLogoImage"
+        onClick={ongoHome}
+      ></img>
+      <LoginController
+        buyerClick={buyerClick}
+        sellerClick={sellerClick}
+        onClickBuyer={onClickBuyer}
+        onClickSeller={onClickSeller}
+      />
+      {buyerClick === true && sellerClick === false ? (
+        <BuyerLogin />
+      ) : (
+        <SellerLogin />
+      )}
+    </div>
+  );
 }
 
 export default Login;
